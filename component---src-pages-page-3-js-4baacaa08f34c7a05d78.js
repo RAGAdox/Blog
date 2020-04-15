5a47c4c2a78094b915e27340fe443af286c1b0dc
RAGAdox <rishitech882@gmail.com>
2020-04-16 03:34:35 +0530

    Updates

---
+++ component---src-pages-page-3-js-4baacaa08f34c7a05d78.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{gWnc:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),c=a("Wbzz"),o=a("Bl7J"),r=a("vrFN");t.default=function(){return l.a.createElement(o.a,null,l.a.createElement(r.a,{title:"Page two"}),l.a.createElement("h1",null,"Hi from the second page"),l.a.createElement("p",null,"Welcome to page 3 "),l.a.createElement("p",null,"This is an example page"),l.a.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-3-js-4baacaa08f34c7a05d78.js.map
